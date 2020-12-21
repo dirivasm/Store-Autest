fd99797db3b9123eb2c4f98f19e8219f826e43b1
dirivasm <62437745+dirivasm@users.noreply.github.com>
2020-12-21 17:31:45 -0600

    Add comments to Setup Class

--- Store Autest/src/main/java/Setup/TestSetup.java
+++ Store Autest/src/main/java/Setup/TestSetup.java
@@ -9,6 +9,13 @@ import javax.rmi.CORBA.Util;
 public class TestSetup {
     private static WebDriver driver;
 
+        /*
+         - Always remember to reference your file in a relative way - /Users/jconcepcion is not a valid path for the project to be portable
+         - Another alternative to avoid this is using below maven dependency (In order to handle the WebDriver):
+
+            <groupId>io.github.bonigarcia</groupId>
+            <artifactId>webdrivermanager</artifactId>
+         */
     public static void Setup() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","/Users/jconcepcion/IdeaProjects/Store Autest/src/main/drivers/chromedriver");
         driver = new ChromeDriver();
