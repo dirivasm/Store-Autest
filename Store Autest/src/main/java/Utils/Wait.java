b02f042e75fdfa1d4c7d4f7895a50bd8f69ab5ac
dirivasm <62437745+dirivasm@users.noreply.github.com>
2020-12-21 17:34:26 -0600

    Added comments to Wait class

--- Store Autest/src/main/java/Utils/Wait.java
+++ Store Autest/src/main/java/Utils/Wait.java
@@ -7,6 +7,11 @@ public final class Wait {
     // Prevents this class from being instantiated
     private Wait() {
     }
+    
+     /*
+       We should use Explicit, Implicit or Fluent wait to handle Selenium interactions.
+       Using Thread.sleep or TimeUnit.X.sleep will unnecessary increase execution time
+     */
 
     public static void until(final int seconds) throws InterruptedException {
         TimeUnit.SECONDS.sleep((long) seconds);
